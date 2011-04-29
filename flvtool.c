8d55c9d5666596805b362ab540063a3963800245
Niv Sardi <xaiki@evilgiggle.com>
2011-04-29 19:37:33 -0300

    New flvtool, now uses parsefile.
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- flvtool.c
+++ flvtool.c
@@ -76,14 +76,17 @@ void refbuf_destroy (refbuf_t *refbuf) {
 	free(refbuf);
 }
 
+void xa1d (char *b) {
+	printf ("b[%d]\n", b[0]);
+}
 int main (int argc, char *argv[]) {
 	int 		fd, bytes, last = 0;
 	ssize_t		len;
-	size_t		offset = 0, readc = 0, tot = 0, fill = 0;
+	size_t		pos = 0, readc = 0, tot = 0, fill = 0, off = 0;
 	struct stat	statb;
 
 	refbuf_t	*refbuf;
-	parseme_t	*h, *t;
+	parsefile_t	*f;
 
 	if (argc < 2) {
 		perror ("Need more arguments");
@@ -101,86 +104,79 @@ int main (int argc, char *argv[]) {
 		return 0;
 	}
 
-	h = mpegparse_new (flv_header);
-	t = mpegparse_new (flv_tag);
+	f = &flv_file;
 
 	refbuf = refbuf_new (4096);
 
-	if (!h || !t || !refbuf) {
+	if (!f || !refbuf) {
 		perror ("memory issue");
 		return 0;
 	}
 
-	fill = read (fd, refbuf->data, refbuf->len); /* not really ideal */
-	if (refbuf->len <= 0) {
-		perror ("couldn't read");
-		return 0;
-	}
-
-	bytes = parse (refbuf->data, fill, h, NULL);
-	if (bytes <= 0)
-		goto err_parse;
-	tot += bytes;
-	readc += bytes;
-
 	while (1) {
-		for (readc = 0; fill + readc < refbuf->len;) {
-			bytes = read (fd, refbuf->data + offset, refbuf->len);
-			if (bytes <= 0) {
-				printf ("tot(%d) + readc(%d) [%d] statb.st_size (%d)\n",
-					tot, readc, tot + readc, statb.st_size);
-				if (tot + readc >= statb.st_size) {
-					last = 1;
-					refbuf->len = readc;
-					break;
-				}
-				goto err_readc;
-			}
-			readc += bytes;
+		dprintf ("trying to read at offset: %d, size %d\n",
+			fill, refbuf->len - fill);
+		bytes = read (fd, refbuf->data + fill, refbuf->len - fill); /* not really ideal */
+		if (bytes <= 0) {
+			printf ("couldn't read at offset %d/%d, size %d\n",
+				readc + fill, statb.st_size, refbuf->len - fill);
+			return 0;
 		}
-		fill += readc;
-		xa1_dump_size (refbuf->data + readc, 8);
 
-		while ((bytes = parse(refbuf->data + readc, refbuf->len - readc, t, NULL)) > 0) {
-			pm_data_type val;
-			if (refbuf->len - bytes - readc < FLV_TAG_SIZE) {
-				xa1_dump_size (refbuf->data + readc, 8);
-				tot += bytes;
-				bytes = -bytes + FLV_TAG_SIZE;
+		fill += bytes;
+
+		char *endptr;
+		while (fill > pos) {
+			dprintf ("parsing at %d, offset %d, readc %d\n", tot, pos, readc);
+			bytes = pm_parse_file (refbuf->data + pos, fill - pos, f, &endptr);
+			if (bytes == 0 || !endptr)
+				goto err_parse;
+
+			tot += endptr - refbuf->data - pos;
+			pos = endptr - refbuf->data;
+			dprintf ("got endptr, pos %d\n", pos);
+
+			if (bytes < 0) {
+				if (-bytes >= refbuf->len) {
+					printf ("refbuf too small, need %d more, reallocing: %d, will copy from offset %d, size %d, fill %d\n",
+						-bytes - refbuf->len, pos - bytes, pos, fill - pos, fill);
+
+					refbuf_t *tmp = refbuf_new(pos - bytes);
+					memcpy (tmp->data, refbuf->data + pos, fill - pos);
+
+					refbuf_destroy (refbuf);
+					refbuf = tmp;
+				} else {
+					dprintf ("not enough data, fetching more, keeping data of size %d at offset %d, total %d\n",
+						fill - pos, pos, refbuf->len);
+					memcpy (refbuf->data, refbuf->data + pos, fill - pos);
+				}
+				readc += pos;
+				fill -= pos;
+				pos = 0;
 				break;
 			}
-
-			if (pm_get_key (t, "PreviousTagSize", &val))
-				printf ("[%d] PrviousTagSize: %d\n", tot, val);
-			xa1_dump_size (refbuf->data + readc, 8);
-
-			readc += bytes;
-			tot += bytes;
+			pm_file_describe_last (f);
+			printf ("\n");
+			dprintf ("PARSING OK left to read: %d\n\n", refbuf->len - fill);
+			if (pos == fill) {/* we filled all we had, and have nothing stray */
+				pos = fill = 0;
+			}
 		}
-		if  (!bytes)
-			goto err_parse;
-
-		offset = refbuf-> len - readc;
-		memmove (refbuf->data, refbuf->data + readc, offset);
-		if (bytes + refbuf->len < 0 && !last) {
-			refbuf = refbuf_realloc (refbuf, -bytes + FLV_TAG_SIZE);
-			fill = offset;
-		}
-
-		printf ("[%d/%d] need %d more \n", tot, statb.st_size, -bytes);
 	}
 
  err_parse:
 	printf ("ERROR: [%d/%d] Couldn't parse (bytes = %d, readc = %d)\n", tot, statb.st_size, bytes, readc);
-	printf("[%d]", tot - 8);
-	xa1_dump_size (refbuf->data + readc -8, 8);
-	printf("[%d]", tot);
-	xa1_dump_size (refbuf->data + readc, 8);
-	printf("[%d]", tot + 8);
-	xa1_dump_size (refbuf->data + readc + 8, 8);
+	printf("[%x]", tot - 8);
+	xa1_dump_size (refbuf->data + tot - readc -8, 8);
+	printf("[%x]", tot);
+	xa1_dump_size (refbuf->data + tot - readc, 8);
+	printf("[%x]", tot + 8);
+	xa1_dump_size (refbuf->data + tot - readc + 8, 8);
 	return 0; /* need to free */
 
  err_readc:
-	printf ("ERROR while reading at %d/%d, read %d needed %d %m\n", tot, statb.st_size, readc, refbuf->len);
+	printf ("ERROR while reading at %d/%d, read %d needed %d %m\n",
+		tot, statb.st_size, readc, refbuf->len);
 	return 0;
 }
